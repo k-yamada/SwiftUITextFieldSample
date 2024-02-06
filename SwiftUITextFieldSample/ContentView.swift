40eb14553ae0945438de23636c5eb50f0ecf46e9
Kazuhiro Yamada <yamadakazu45@gmail.com>
2024-02-06 14:33:41 +0900

    Initial Commit

---
+++ SwiftUITextFieldSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITextFieldSample
+//
+//  Created by KazuhiroYamada on 2024/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITextFieldSample/SwiftUITextFieldSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextFieldSampleApp.swift
+//  SwiftUITextFieldSample
+//
+//  Created by KazuhiroYamada on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextFieldSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
